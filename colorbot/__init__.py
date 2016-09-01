1f0b43f3b0127c8f9e43d4ecc8a3b80ddcf01e4f
Wes Weber <wweber@wesweber.com>
2016-09-01 16:37:06 -0400

    Adjust logging.

--- colorbot/__init__.py
+++ colorbot/__init__.py
@@ -7,10 +7,13 @@ formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
 handler.setFormatter(formatter)
 
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.WARNING)
 root_logger.addHandler(handler)
 
 colorbot_logger = logging.getLogger("colorbot")
+colorbot_logger.setLevel(logging.DEBUG)
+colorbot_logger.addHandler(handler)
+colorbot_logger.propagate = False
 
 tensorflow_logger = logging.getLogger("tensorflow")
 tensorflow_logger.setLevel(logging.WARNING)
