977cb65cf6f9a36c2e91b8d4dbca7d89586cb7a4
Wes Weber <wweber@wesweber.com>
2016-09-03 18:00:29 -0400

    Add some debug messages.

--- colorbot/twitter/bot.py
+++ colorbot/twitter/bot.py
@@ -166,13 +166,16 @@ def guess_color(name, status_id, screen_name, global_state):
 class StreamListener(tweepy.StreamListener):
     def __init__(self, state):
         self.state = state
+        self.api = self.state.api
         self._my_id = None
 
     @property
     def my_id(self):
         if self._my_id is None:
             with self.state.lock:
+                logger.debug("Looking up own ID")
                 me = self.state.api.me()
+                logger.debug("We are %s (%s)" % (me.id_str, me.screen_name))
             self._my_id = me.id_str
 
         return self._my_id
@@ -181,11 +184,16 @@ class StreamListener(tweepy.StreamListener):
         logger.error("Stream received error code %d" % status_code)
         return False
 
+    def on_disconnect(self, notice):
+        return False
+
     def on_status(self, status):
         id = status.id_str
         author_id = status.user.id_str
         screen_name = status.user.screen_name
         msg = status.text
+
+        logger.debug("Got status: %s" % msg)
 
         if author_id == self.my_id:
             # Ignore, this is our own tweet
@@ -240,10 +248,13 @@ def worker(state):
                 state.has_tasks.wait()
 
             if state.stop is not False:
+                logger.debug("Worker thread exiting")
                 return  # Stop
 
             # get task
             task = state.tasks.pop()
+
+            logger.debug("Worker thread handling task: %r" % task)
 
             if task["type"] == "name":
                 name_color(task["hex"], task["status_id"], task["screen_name"],
@@ -266,17 +277,23 @@ def run(auth, name_set, api, vocab, hidden_size, param_path):
 
     def int_handler(*args):
         with state.has_tasks:
+            logger.info("Stopping")
             state.stop = True
             state.has_tasks.notify_all()
-
-        stream.disconnect()
 
     signal.signal(signal.SIGINT, int_handler)
     signal.signal(signal.SIGTERM, int_handler)
 
+    logger.info("Starting worker thread")
     worker_thread = threading.Thread(target=worker, args=(state,))
-    worker_thread.run()
+    worker_thread.start()
 
+    logger.info("Starting twitter stream")
     stream.userstream(async=True)
 
     worker_thread.join()
+
+    stream.disconnect()
+
+    logger.info("Exiting")
+    exit(0)
